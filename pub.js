112fe37b44c87e274b69363495b93aed1d31fb20
gorithub <tanimasaru@hotmail.com>
2014-07-09 00:49:54 +0900

    Update pub.js
    comment change

--- pub.js
+++ pub.js
@@ -1,7 +1,7 @@
 redis = require('redis'),
 client = redis.createClient(6379, 'localhost');
 
-// 1秒おきにランダムにチャンネル名と数値を送信する
+// random number by 1 minutes
 setInterval(function() {
   client.publish(
     "hoge channel",
